c56f8d4ce84ba7eb77db82ceca120c94b85f6339
hey-kong <iggiewang@gmail.com>
2022-07-05 15:26:05 +0800

    support json

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 import re
 
-template_path = 'vehicle_template.txt'
-config_path = 'vehicle.txt'
+vehicle_template_path = 'vehicle_template.txt'
+vehicle_config_path = 'vehicle.txt'
+
+msgconv_template_path = 'msgconv_template.txt'
+msgconv_config_path = 'msgconv.txt'
 
 source_template = [
     '[source$num]\n',
@@ -13,6 +16,16 @@ source_template = [
     'gpu-id=0\n',
     'cudadec-memtype=0\n',
     'num-extra-surfaces=2\n'
+]
+
+sensor_template = [
+    '[sensor$num]\n',
+    'enable=1\n',
+    'type=Camera\n'
+    'id=$rtsp_id\n',
+    'description=Vehicle Detection and License Plate Recognition\n',
+    '#location=\n',
+    '#coordinate=\n'
 ]
 
 
@@ -27,26 +40,46 @@ def gen_source(sources, urls):
     return source_config
 
 
-def gen_config(rows, columns, sources, urls):
-    vehicle_template = open(template_path, 'r')
-    vehicle = open(config_path, 'w+')
+def gen_sensor(sources, ids):
+    sensor_config = []
+    for i in range(sources):
+        for line in sensor_template:
+            line = re.sub(r'\$num', str(i), line)
+            line = re.sub(r'\$rtsp_id', ids[i], line)
+            sensor_config.append(line)
+        sensor_config.append('\n')
+    return sensor_config
+
+
+def gen_vehicle_config(rows, columns, sources, urls, host, port, topic):
+    vehicle_template = open(vehicle_template_path, 'r')
+    vehicle = open(vehicle_config_path, 'w+')
     row = r'\$rows'
     row_val = str(rows)
     col = r'\$columns'
     col_val = str(columns)
     batch = r'\$batch'
     batch_val = str(sources)
+    host = r'\$host'
+    host_val = str(host)
+    port = r'\$port'
+    port_val = str(port)
+    topic = r'\$topic'
+    topic_val = str(topic)
     for line in vehicle_template.readlines():
         line = re.sub(row, row_val, line)
         line = re.sub(col, col_val, line)
         line = re.sub(batch, batch_val, line)
+        line = re.sub(host, host_val, line)
+        line = re.sub(port, port_val, line)
+        line = re.sub(topic, topic_val, line)
         vehicle.write(line)
     vehicle_template.close()
     vehicle.seek(0)
     all_lines = vehicle.readlines()
     vehicle.close()
 
-    vehicle = open(config_path, 'w+')
+    vehicle = open(vehicle_config_path, 'w+')
     for line in all_lines:
         if line == "{{sources}}\n":
             source_config = gen_source(sources, urls)
@@ -56,10 +89,29 @@ def gen_config(rows, columns, sources, urls):
     vehicle.close()
 
 
+def gen_msgconv_config(sources, ids):
+    msgconv_path = open(msgconv_template_path, 'r')
+    msgconv = open(msgconv_config_path, 'w+')
+    for line in msgconv_path.readlines():
+        if line == "{{sensor}}\n":
+            sensor_config = gen_sensor(sources, ids)
+            msgconv.writelines(sensor_config)
+        else:
+            msgconv.write(line)
+    msgconv.close()
+
+
 if __name__ == "__main__":
     rows = int(os.environ.get("rows"))
     columns = int(os.environ.get("columns"))
     sources = int(os.environ.get("sources"))
+    ids = str(os.environ.get("rtsp_ids")).split(',')
     urls = str(os.environ.get("rtsp_urls")).split(',')
-    gen_config(rows, columns, sources, urls)
+    host = int(os.environ.get("host"))
+    port = int(os.environ.get("port"))
+    topic = int(os.environ.get("topic"))
+    # vehicle.txt
+    gen_vehicle_config(rows, columns, sources, urls, host, port, topic)
+    # msgconv.txt
+    gen_msgconv_config(sources, ids)
     os.system('deepstream-app -c vehicle.txt')
